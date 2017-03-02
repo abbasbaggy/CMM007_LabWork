e079539385c5ebaa2e405bf0665fc7c7e915eeec
abbasbaggy <a.yusuff@rgu.ac.uk>
2017-03-02 23:34:38 +0000

    php practice

--- week5/helloprinter.php
+++ week5/helloprinter.php
@@ -5,8 +5,8 @@
  * Date: 3/2/2017
  * Time: 10:33 PM
  */
-echo  $_GET['subject']. $_GET[web];
+echo  $_GET['subject']. " ".$_GET[web];
 echo "<br>";
-echo $_GET["mar"]. $_GET[salute];
+echo $_GET["mar"]. " " .$_GET[salute];
 echo "<br>";
-echo $_GET[anus]. $_GET[name]. $_GET[good];
+echo $_GET[anus]. " " .$_GET[name]. " " .$_GET[good];
