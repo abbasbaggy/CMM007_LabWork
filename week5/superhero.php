13b8bba169a4b97c33dc11b8ede4bb000cbf4b59
abbasbaggy <a.yusuff@rgu.ac.uk>
2017-03-03 11:19:50 +0000

    php practice

---
+++ week5/superhero.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Abbas
+ * Date: 3/3/2017
+ * Time: 10:39 AM
+ */
+$fname = $_POST["fname"];
+$sname =$_POST["sname"];
+$gender = $_POST["submit"];
+$power =$_POST["power"];
+
+echo "Firstname ". $fname."surname ". $sname."Gender ". $gender. "My super Powers ";
