ce29e0e7398871c1645d41871606a4748a570cd4
abbasbaggy <a.yusuff@rgu.ac.uk>
2017-03-03 10:26:13 +0000

    php practice

---
+++ week5/formResponse.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Abbas
+ * Date: 3/3/2017
+ * Time: 10:23 AM
+ */
+$forename = $_POST["forename"];
+$surname = $_POST["surname"];
+echo $forename. " ". $surname;
---
+++ week5/usingforms.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>using forms</head>
+
+<body>
+
+<head>
+    <h2>We are learning php using forms</h2>
+</head>
+
+<main>
+
+    <form action ="formResponse.php" method="post">
+    <lable>Forename</lable><input type="text" name="forename">
+    <label>Surname</label><input type="text" name="surname">
+    <input type="submit" value="submit">
+</form>
+
+</main>
+</body>
+
+
+
+</html>
+
+
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Abbas
+ * Date: 3/3/2017
+ * Time: 10:12 AM
+ */
+
