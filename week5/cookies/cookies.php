c4f638621213f956a1ad29aa833ceec59b09c88e
abbasbaggy <a.yusuff@rgu.ac.uk>
2017-03-07 23:44:55 +0000

    php practice

--- week5/cookies/cookies.php
+++ week5/cookies/cookies.php
@@ -13,8 +13,8 @@ if (isset($_POST["username"]) && isset($_POST["password"])){
 
         if (isset($_POST["rememberme"])) {
 
-            setcookie("username", $_POST["username"], time() + 60 * 60 * 24 * 365, "/account");
-            setcookie("password", $_POST["password"], time() + 60 * 60 * 24 * 365, "/account");
+            setcookie("username", $_POST["username"], time() + 60 * 60 * 24 * 365);
+            setcookie("password", $_POST["password"], time() + 60 * 60 * 24 * 365);
 
         } else {
 
