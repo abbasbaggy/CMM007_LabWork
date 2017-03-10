ff818ccf743f6955678e64b17c57f4a6966f725f
abbasbaggy <a.yusuff@rgu.ac.uk>
2017-03-10 23:56:19 +0000

    php practices

--- week6/db_connect.php
+++ week6/db_connect.php
@@ -27,4 +27,7 @@ if (!$link) {
     exit;
 }
 
-echo "$connectstr_dbhost"," ","$connectstr_dbname"," ","$connectstr_dbusername"," ","$connectstr_dbpassword";
+echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
+echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
+
+mysqli_close($link);
