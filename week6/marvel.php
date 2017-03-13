6711dbbf1796dc5a0aa864f6e86301e72fd0ea26
abbasbaggy <a.yusuff@rgu.ac.uk>
2017-03-13 23:18:58 +0000

    php practices

--- week6/marvel.php
+++ week6/marvel.php
@@ -10,19 +10,16 @@ include ('db_connect.php');
 // create a SQL query as a string
 $sql_query = "SELECT * FROM localdb.marvelmovies";
 // execute the SQL query
-$result = $db->query($sql_query);
+$result=mysqli_query($db,$sql_query);
+
 
 // iterate over $result object one $row at a time
 // use fetch_array() to return an associative array
 while($row = $result->fetch_array()){
 // process the $row
+    $movieTitle =$row['title'];
+    echo "<p>" .$movieTitle. "</p>";
 }
 
-while($row = $result->fetch_array()){
-    // print out fields from row of data
-    echo "<p>” . $row{'marvelMovies'} . “</p>";
-}
 
-$result->close();
-// close connection to database
-$db->close();
+
