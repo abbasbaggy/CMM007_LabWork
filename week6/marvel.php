ec31e21f62356e7f88e22b01bb5a35374853fc9c
abbasbaggy <a.yusuff@rgu.ac.uk>
2017-03-13 23:23:31 +0000

    php practices

--- week6/marvel.php
+++ week6/marvel.php
@@ -5,10 +5,10 @@
  * Date: 07/03/2017
  * Time: 16:27
  */
-include ('db_connect.php');
+include('db_connect.php');
 
 // create a SQL query as a string
-$sql_query = "SELECT * FROM localdb.marvelmovies";
+$sql_query = "SELECT * FROM myfisrtpage.marvelmovies";
 // execute the SQL query
 $result=mysqli_query($db,$sql_query);
 
