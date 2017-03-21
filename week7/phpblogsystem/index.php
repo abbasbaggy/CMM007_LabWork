b673924cc1bd8e0fe2dbeca613fa535199734f3a
abbasbaggy <a.yusuff@rgu.ac.uk>
2017-03-21 22:11:09 +0000

    week7

--- week7/phpblogsystem/index.php
+++ week7/phpblogsystem/index.php
@@ -9,26 +9,26 @@ define('INCLUDE_DIR', dirname(__FILE__) . '/inc/');
 
 $rules = array(
     //main pages
-    "about" => "/about",
-    "contactus" => "/contactus",
-    "blog" => "/blog",
-    "blog_article" => "/blog/(?'blogID'[\w\-]+)",
+    'about' => "/about",
+    'contactus' => "/contactus",
+    'blog' => "/blog",
+    'blog_article' => "/blog/(?'blogID'[\w\-]+)",
 
     //Admin pages
-    "login" => "/login",
-    "create_article" => "/createarticle",
-    "logout" => "/logout",
+    'login' => "/login",
+    'create_article' => "/createarticle",
+    'logout' => "/logout",
 
     //Home page
-    "home" => "/"
+    'home' => "/"
 );
-$uri = rtrim(dirname($_SERVER["SCRIPT_NAME"]), "/");
-$uri ="/" . trim(str_replace($uri, '', $_SERVER['REQUEST_URI']), "/");
+$uri = rtrim(dirname($_SERVER["SCRIPT_NAME"]), '/');
+$uri = '/' . trim(str_replace($uri, ' ', $_SERVER['REQUEST_URI']), '/');
 $uri = urldecode($uri);
 
 foreach ($rules as $action => $rule) {
-    if (preg_match("~^" . $rule . "$~i", $uri, $params)) {
-        include(INCLUDE_DIR . $action . ".php");
+    if (preg_match('~^' . $rule . '$~i', $uri, $params)) {
+        include(INCLUDE_DIR . $action . '.php');
         exit();
     }
 }
