700cdeee04c81ef182c2e8509e498906bd0dc090
abbasbaggy <a.yusuff@rgu.ac.uk>
2017-03-21 22:06:51 +0000

    week7

--- week7/phpblogsystem/index.php
+++ week7/phpblogsystem/index.php
@@ -22,13 +22,13 @@ $rules = array(
     //Home page
     "home" => "/"
 );
-$uri = rtrim(dirname($_SERVER["SCRIPT_NAME"]), '/');
-$uri ='/' . trim(str_replace($uri, '', $_SERVER['REQUEST_URI']), '/');
+$uri = rtrim(dirname($_SERVER["SCRIPT_NAME"]), "/");
+$uri ="/" . trim(str_replace($uri, '', $_SERVER['REQUEST_URI']), "/");
 $uri = urldecode($uri);
 
 foreach ($rules as $action => $rule) {
-    if (preg_match('~^' . $rule . '$~i', $uri, $params)) {
-        include(INCLUDE_DIR . $action . '.php');
+    if (preg_match("~^" . $rule . "$~i", $uri, $params)) {
+        include(INCLUDE_DIR . $action . ".php");
         exit();
     }
 }
