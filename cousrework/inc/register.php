660c7de70d15926a123b3d9965e5d5a73ea2bf7b
abbasbaggy <a.yusuff@rgu.ac.uk>
2017-03-29 22:05:53 +0100

    course work

--- cousrework/inc/register.php
+++ cousrework/inc/register.php
@@ -90,7 +90,7 @@ if ( isset($_POST['btn-signup']) ) {
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <title>Exculsive Fashion</title>
-    <link rel="stylesheet" href="assets/css/bootstrap.min.css" type="text/css"  />
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" type="text/css"  />
     <link rel="stylesheet" href="style.css" type="text/css" />
 </head>
 <body>
