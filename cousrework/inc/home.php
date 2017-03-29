caa6effe3df6a04be6091638e3c4c63a39e778be
abbasbaggy <a.yusuff@rgu.ac.uk>
2017-03-29 21:14:04 +0100

    course work

--- cousrework/inc/home.php
+++ cousrework/inc/home.php
@@ -16,7 +16,7 @@ if (!isset($_SESSION['user']) ) {
  //User logged in details
  $sql_query="SELECT * FROM users WHERE userId=".$_SESSION['user'];
 $row =$link->query($sql_query);
-$userRow=mysqli_fetch_array($row);
+$userRow=mysqli_num_rows($row);
  ?>
 <!DOCTYPE html>
 <html>
