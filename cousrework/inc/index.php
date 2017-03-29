e90baf7e0d2893c9ff68eab34cc79ead1fb17887
abbasbaggy <a.yusuff@rgu.ac.uk>
2017-03-29 18:29:42 +0100

    course work

--- cousrework/inc/index.php
+++ cousrework/inc/index.php
@@ -47,8 +47,8 @@ if( isset($_POST['btn-login']) ) {
 
         $password = hash('sha256', $pass); // password hashing using SHA256
 
-        $sql_query =mysqli_query("SELECT userId, userName, userPass FROM users WHERE userEmail='$email'");
-        $row=mysqli_fetch_array($sql_query);
+        $sql_query ="SELECT userId, userName, userPass FROM users WHERE userEmail='$email'";
+        $row=$link->query($sql_query);
         $count = mysqli_num_rows($row); // if uname/pass correct it returns must be 1 row
 
         if( $count == 1 && $row['userPass']==$password ) {
