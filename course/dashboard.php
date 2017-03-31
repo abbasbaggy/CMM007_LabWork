415da2601312fcb0394d8fd8f81cd65b060bce96
abbasbaggy <a.yusuff@rgu.ac.uk>
2017-03-31 18:26:42 +0100

    course work

--- course/dashboard.php
+++ course/dashboard.php
@@ -17,20 +17,18 @@ include("auth.php"); //include auth.php file on all secure pages ?>
 <html>
 <head>
     <meta charset="utf-8">
-    <title>Dashboard - Secured Page</title>
+    <title>Exclusive fashion- ur logged in</title>
     <link rel="stylesheet" href="css/style.css" />
 </head>
 <body>
 <div class="form">
-    <p>Dashboard</p>
-    <p>This is another secured page.</p>
+    <p>Tailoring Design</p>
     <p><a href="index.php">Home</a></p>
-    <a href="logout.php">Logout</a>
+    <p><a href="insert.php">Insert New Records</a></p>
+    <p><a href="view.php">View Records</a></p>
+    <p><a href="logout.php">Logout</a></p>
 
 
-    <br /><br /><br /><br />
-    <a href="http://www.allphptricks.com/simple-user-registration-login-script-in-php-and-mysqli/">Tutorial Link</a> <br /><br />
-    For More Web Development Tutorials Visit: <a href="http://www.allphptricks.com/">AllPHPTricks.com</a>
 </div>
 </body>
 </html>
