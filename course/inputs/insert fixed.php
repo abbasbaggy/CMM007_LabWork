d3bac960dfd1aab1c8c949d16f64dc1cbd29f83d
abbasbaggy <a.yusuff@rgu.ac.uk>
2017-04-02 15:10:42 +0100

    course work

--- course/inputs/insert fixed.php
+++ course/inputs/insert fixed.php
@@ -31,16 +31,20 @@ $status = "";
     <meta charset="utf-8">
     <title>Insert New Measurement</title>
     <link rel="stylesheet" href="../css/style.css" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
 <header>
     <h2> Sample for header</h2>
 </header>
 <body>
-<div class="form2">
+<div class="form-inline">
     <p><a href="../dashboard.php">Dashboard</a>
         | <a href="../view.php">View Records</a>
         | <a href="../logout.php">Logout</a></p>
-    <div>
+    <div class="form-group">
         <h1>Please Insert Your Measurements</h1>
         <form name="form" method="post" action="">
             <input type="hidden" name="new" value="1" />
