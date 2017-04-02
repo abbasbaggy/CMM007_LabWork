54cd8282bb2294346c0390ee8221534ec9be7440
abbasbaggy <a.yusuff@rgu.ac.uk>
2017-04-02 14:31:23 +0100

    course work

--- course/dashboard.php
+++ course/dashboard.php
@@ -19,9 +19,13 @@ include("auth.php"); //include auth.php file on all secure pages ?>
     <meta charset="utf-8">
     <title>Exclusive fashion- ur logged in</title>
     <link rel="stylesheet" href="css/style.css" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
 <header>
-<div class="form">
+<div class="container">
     <p>Tailoring Design</p>
     <p><a href="index.php">Home</a></p>
     <p><a href="insert.php">Insert New Records</a></p>
