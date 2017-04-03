b336ce905fbfae0dcda1d49167ec34b0c0b3871a
abbasbaggy <a.yusuff@rgu.ac.uk>
2017-04-03 14:55:56 +0100

    course work

--- course/view.php
+++ course/view.php
@@ -7,20 +7,12 @@
  */
 require('db.php');
 include("auth.php");
+include ('dashboard.php')
 ?>
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8">
-    <title>View Records</title>
-    <link rel="stylesheet" href="css/style.css" />
-</head>
+
 <body>
 <main>
-<div class="form">
-    <p><a href="index.php">Home</a>
-        | <a href="insert.php">Insert New Record</a>
-        | <a href="logout.php">Logout</a></p>
+
     <h2>View Records</h2>
     <table width="100%" border="1" style="border-collapse:collapse;">
         <thead>
