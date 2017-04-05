af233a698f2c9624cd75ccca64231e8ca3977634
abbasbaggy <a.yusuff@rgu.ac.uk>
2017-04-05 10:21:44 +0100

    course work

--- course/login.php
+++ course/login.php
@@ -33,7 +33,7 @@ if (isset($_POST['username'])){
 
     //Checking is user existing in the database or not
     $query = "SELECT * FROM `users2` WHERE username='$username' and password='".md5($password)."'";
-    $result = mysqli_query($con,$query) or die(mysql_error());
+    $result = mysqli_query($con,$query) or die(mysqli_error());
     $rows = mysqli_num_rows($result);
     if($rows==1){
         $_SESSION['username'] = $username;
